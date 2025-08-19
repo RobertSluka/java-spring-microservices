1ebd6af17d4383ad2e76b8bb2a57378d1859b228
r.sluka <robosluka11@gmail.com>
2025-08-19 12:01:47 +0200

    Added websocket logic with frontend UI

---
+++ chat-service/src/main/java/org/example/chatservice/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package org.example.chatservice.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+}
---
+++ chat-service/src/main/java/org/example/chatservice/chat/MessageType.java
@@ -0,0 +1,7 @@
+package org.example.chatservice.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
